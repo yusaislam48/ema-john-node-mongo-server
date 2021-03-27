735f75d96b22ebb9ab343979342d1c1f7589e0c6
Yusa Islam <yusaislam48@gmail.com>
2021-03-28 03:20:24 +0600

    .env edit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json());
 app.use(cors())
 const port = 4000;
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.qsbja.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qsbja.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
