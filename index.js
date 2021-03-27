fa73aa0d257b7734e26865b35661f4a3e6599955
Yusa Islam <yusaislam48@gmail.com>
2021-03-28 03:26:04 +0600

    root api created

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.get('/', (req, res) => {
+    res.send("hello from db & it's working!")
+})
+
 
 client.connect(err => {
   const productsCollection = client.db("emajohnStore").collection("products");
